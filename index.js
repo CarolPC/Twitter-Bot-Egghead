add1243ac49a22081b3e6c9d86e1b35e218988d3
CarolPC <carolina.pascale.c@gmail.com>
2017-05-14 22:07:49 -0300

    First tweet posting status to the statuses/update endpoint

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const bot = new Twit({
     consumer_key: process.env.CONSUMER_KEY,
     consumer_secret: process.env.CONSUMER_SECRET,
     access_token: process.env.ACCESS_TOKEN,
-    access_token_secret: process.env.CONSUMER_SECRET,
+    access_token_secret: process.env.ACCESS_TOKEN_SECRET,
     timeout_ms
 });
+
+bot.post('statuses/update', { status: 'I am Batman' })
+.then((result) => console.log(`${result.data.text} was tweeted`))
+.catch((error) => console.log(`${JSON.stringify(error)}`));
