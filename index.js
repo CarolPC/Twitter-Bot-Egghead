d5ec64d26e2ef47bab169370db1ab8a58aea5140
CarolPC <carolina.pascale.c@gmail.com>
2017-05-14 23:32:08 -0300

    Create actions

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
 require('dotenv').config();
-const PostTweet = require('./app/services/tweet');
+const { postTweet, sendDm } = require('./app/services/tweet');
+const { follow, getFriends, getConnections } = require('./app/services/follow');
+const { getFollowersIds, getFollowersList } = require('./app/services/followers');
 
-PostTweet('hello 2');
+const Actions = {
+    tweet: (message) => postTweet(message),
+    dm: (user, message) => sendDm(user, message),
+    followersIds: getFollowersIds(),
+    followersList: getFollowersList(),
+    followUser: (user) => follow(user),
+    friends: getFriends(),
+    connections: getConnections()
+};
+
+Actions.tweet('hello');
+Actions.followUser('ahandvanish');
+Actions.followersIds;
+Actions.followersList;
+Actions.friends;
+Actions.connections;
+Actions.dm('anyone', 'batman');
