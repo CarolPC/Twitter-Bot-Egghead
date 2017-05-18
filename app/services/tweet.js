f929ba29b3c42a9f2ccdde9f36a116fbe3a363de
CarolPC <carolina.pascale.c@gmail.com>
2017-05-17 22:13:06 -0300

    Add retweet, unretweet, favorite and reply function

--- app/services/tweet.js
+++ app/services/tweet.js
@@ -15,4 +15,28 @@ const sendDm = (user, message) => {
     .then((result) => console.log(result.data));
 };
 
-module.exports = { postTweet, sendDm };
+const retweet = (id) => {
+
+    return bot.post('statuses/retweet/:id', { id })
+    .then((result) => console.log(`${result.data} was retweeted`));
+};
+
+const unretweet = (id) => {
+
+    return bot.post('statuses/unretweet/:id', { id })
+    .then((result) => console.log(`${result.data.text} was unretweeted`));
+};
+
+const favorite = (id) => {
+
+    return bot.post('favorites/create/:id', { id })
+    .then((result) => console.log(result.data));
+};
+
+const reply = (id) => {
+
+    return bot.post('statuses/update', { status: '@ahandvanish hi!', in_reply_to_statys_id: id })
+    .then((result) => console.log(result.data));
+};
+
+module.exports = { postTweet, sendDm, retweet, unretweet, favorite, reply };
